b4217b133b9266fa6e5ec24c270f7c68e95695f9
priyanshuspawar <priyanshusingh306@gmail.com>
2022-09-12 21:40:10 +0530

    reac9t ntive video

--- src/screens/Content/index.tsx
+++ src/screens/Content/index.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import Video from 'react-native-video';
+import { vh, vw } from '../../utils/dimension';
 
 export default function Content(props:any) {
   return (
     <View>
+      <Video source={{uri:"http://techslides.com/demos/sample-videos/small.mp4"}} style={{height:vh(100),width:vw(100)}}/>  
     </View>
   )
 }
